2e2ffd9c89531ead7c6334bb49b02da94e53f316
Christian Mittendorf <christian.mittendorf@freenet.de>
2013-12-02 14:14:36 +0100

    Initial Commit

---
+++ StoryBoardLinkingWithRevealControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryBoardLinkingWithRevealControllerDemo
+//
+//  Created by Christian Mittendorf on 02.12.13.
+//  Copyright (c) 2013 Christian Mittendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryBoardLinkingWithRevealControllerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardLinkingWithRevealControllerDemo
+//
+//  Created by Christian Mittendorf on 02.12.13.
+//  Copyright (c) 2013 Christian Mittendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
